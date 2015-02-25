54fd462becd46bb58c780fef27c434483cac6be8
HE Shi-Jun <hax@heshijun.net>
2015-02-25 12:39:09 +0800

    support stream.Readable

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 'use strict'
 
 var semi = require('semi')
+var Readable = require('stream').Readable
+
 module.exports = semicolonless
 
 function semicolonless(source, options) {
-	return semi.remove(source, {leading: true})
+	if (typeof source === 'string') return semi.remove(source, {leading: true})
+	if (source instanceof Readable) {
+		var result = new Readable()
+		result._read = function noop() {}
+		var sourceStr = ''
+		source.setEncoding('utf-8')
+		source.on('data', function (chunk) {
+			sourceStr += chunk
+		})
+		source.on('end', function () {
+			result.push(semi.remove(sourceStr, {leading: true}))
+			result.push(null)
+		})
+		return result
+	}
 }
-
