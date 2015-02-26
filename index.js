2ae3e10cd50ab0d81635aabb4b82c4a2d6037687
HE Shi-Jun <hax@heshijun.net>
2015-02-26 12:28:15 +0800

    improve

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 'use strict'
 
 var semi = require('semi')
+
+function removeSemicolons(source) {
+	return semi.remove(source, {leading: true})
+}
+
+
 var Readable = require('stream').Readable
 
 module.exports = semicolonless
-
 function semicolonless(source, options) {
-	if (typeof source === 'string') return semi.remove(source, {leading: true})
+
+	if (typeof source === 'string') {
+		return removeSemicolons(source)
+	}
+
+	if (Buffer.isBuffer(source)) {
+		return new Buffer(removeSemicolons(source.toString()))
+	}
+
 	if (source instanceof Readable) {
-		var result = new Readable()
-		result._read = function noop() {}
+		var result = new Readable({
+			read: function () {}
+		})
 		var sourceStr = ''
-		source.setEncoding('utf-8')
+		source.setEncoding('utf8')
 		source.on('data', function (chunk) {
 			sourceStr += chunk
 		})
@@ -21,4 +35,5 @@ function semicolonless(source, options) {
 		})
 		return result
 	}
+
 }
