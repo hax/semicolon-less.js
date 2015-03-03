d7d2c2724a8beac8138ff98789a3b338d0d4ffc0
HE Shi-Jun <hax@heshijun.net>
2015-03-03 15:51:37 +0800

    test

--- test/gulp.js
+++ test/gulp.js
@@ -5,10 +5,6 @@ var Readable = require('readable-stream').Readable
 
 describe('gulpplugin', function () {
 
-	it('should fail', function (done) {
-
-	})
-
 	it('should convert buffer to semicolon-less style', function (done) {
 
 		var testCode = "(function () {\n\t'use strict';\n\tconsole.log('Hello world!');\n})();"
